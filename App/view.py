03f5043e0a44c615968ebdc9e26928cf7f24f615
Rigras <pabloaguati@gmail.com>
2021-02-24 17:52:42 -0500

    cambios x

--- App/view.py
+++ App/view.py
@@ -25,6 +25,7 @@ import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
+import time
 
 
 """
@@ -49,12 +50,20 @@ reducido= None
 """
 Menu principal
 """
+print("cargando")
+start_time = time.process_time()
+catalog=controller.initCatalog("SINGLE_LINKED")
+controller.loadData(catalog)
+stop_time = time.process_time()
+elapsed_time_mseg = (stop_time - start_time)*1000
+print(elapsed_time_mseg)
 while True:
     printMenu()
     tipo=""
     inputs = int(input('Seleccione una opción para continuar '))
     if inputs==0:
-        x=int(input("Presione 1 para seleccionar arreglos, o 2 para seleccionar listas encadenadas"))
+        x=0
+        #x=int(input("Presione 1 para seleccionar arreglos, o 2 para seleccionar listas encadenadas"))
         if x==1:
             tipo="ARRAY_LIST"
         else:
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Estudiante1: Requerimiento 1 y 4 Pablo Alejandro Guatibonza Briceño 202014393 p
 
 Estudiante2 :  Requerimiento 2 y 3 Kaled Andres Correal Sosa   201920829 K.correal@uniandes.edu.co
 
+Modificamos de 512000 a 375918 datos debido a que es el numero maximo de datos en el archivo csv
+
 Este proyecto contiene los Tipos abstractos de datos, estructuras de datos y algoritmos requeridos por el curso ISIS1225-Estructuras de Datos y Algoritmos
 
 -Lib
